4f732df5d0ab06d2842c48318d996ec6f9f7e2f8
CubeBata <romateddy1@gmail.com>
2026-09-12 07:39:55 +0300

    3

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+#define Opt = 0
+#if Opt == 0
+int Main() {
+    std::cout << 0 << std::endl;
+    return 0;
+}
+#if Opt == 1
+int Main() {
+    std::cout << 1 << std::endl;
+    return 0;
+}
+#endif
 int main()
 {
     return  Main();
